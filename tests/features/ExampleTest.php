5596d386a8560d50964d2c18be1f7f0e89b8b0b7
Francisco Padilla Muñoz <padilla.fco@gmail.com>
2017-08-30 15:41:17 +0100

    Setting phpunit tests
    Add Feature test class

--- tests/features/ExampleTest.php
+++ tests/features/ExampleTest.php
@@ -1,17 +1,11 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+
+class ExampleTest extends FeatureTestCase
 {
-    use DatabaseTransactions;
-    /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
+
+    
     public function testBasicExample()
     {
         $user = factory(\App\User::class)->create([
