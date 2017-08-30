118d2c0d9577a460a7ef59e3848edb917306021a
Francisco Padilla Muñoz <padilla.fco@gmail.com>
2017-08-30 15:41:39 +0100

    setting phpunit tests

---
+++ tests/FeatureTestCase.php
@@ -0,0 +1,14 @@
+<?php
+
+use \Illuminate\Foundation\Testing\DatabaseTransactions;
+
+
+class FeatureTestCase extends TestCase
+{
+	use DatabaseTransactions;
+
+	function __construct(argument)
+	{
+		# code...
+	}
+}
